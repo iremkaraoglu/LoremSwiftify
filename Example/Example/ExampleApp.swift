381a46a83e7cf97b5158c2f7534ebd6899cf52d3
iremkaraoglu <iremk97@gmail.com>
2024-03-28 10:56:17 +0300

    Add example project

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Irem Karaoglu on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
